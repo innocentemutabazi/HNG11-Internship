f282372b59df9d8e132d74db28dee7ef8a44eb19
innocentemutabazi <innocentemutabazi@gmail.com>
2024-07-03 17:45:38 +0200

    .

--- Front-end/stage-1/main.js
+++ Front-end/stage-1/main.js
@@ -2,18 +2,17 @@ function updateTime() {
     const now = new Date();
     const days = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
 
-    // Get the current time in UTC
     const utcHours = now.getUTCHours();
     const utcMinutes = now.getUTCMinutes();
     const utcSeconds = now.getUTCSeconds();
     const utcDay = days[now.getUTCDay()];
 
     const utcAmpm = utcHours >= 12 ? 'PM' : 'AM';
-    const adjustedUtcHours = utcHours % 12 || 12; // Convert hours to 12-hour format
+    const adjustedUtcHours = utcHours % 12 || 12;
 
     const utcTime = `${adjustedUtcHours}:${utcMinutes < 10 ? '0' + utcMinutes : utcMinutes}:${utcSeconds < 10 ? '0' + utcSeconds : utcSeconds} ${utcAmpm} ${utcDay} UTC`;
     document.getElementById('current-time').textContent = utcTime;
 }
 
 updateTime();
-setInterval(updateTime, 1000); // Update time every second
+setInterval(updateTime, 1000);
